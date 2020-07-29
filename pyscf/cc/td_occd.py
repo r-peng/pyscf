cbe6ab172b41d5f737d16e2e35c06fca9b031400
Ruojing Peng <rppeng@pauling.caltech.edu>
2020-07-28 22:10:17 -0700

    structure changed

--- pyscf/cc/td_occd.py
+++ pyscf/cc/td_occd.py
@@ -26,60 +26,64 @@ def sort2(tup, anti):
         out[1::2, ::2, ::2,1::2] = - ab.transpose(1,0,2,3).copy()
     return out
 
-def update_t(t, eris):
-    Foo  = eris.foo.copy()
-    Foo += 0.5 * einsum('klcd,cdjl->kj',eris.oovv,t)
-    Fvv  = eris.fvv.copy()
-    Fvv -= 0.5 * einsum('klcd,bdkl->bc',eris.oovv,t)
+def update_t(t, f, eri):
+    no = t.shape[3]
 
-    dt  = eris.oovv.transpose(2,3,0,1).conj().copy()
+    Foo  = f[:no,:no].copy()
+    Foo += 0.5 * einsum('klcd,cdjl->kj',eri[:no,:no,no:,no:],t)
+    Fvv  = f[no:,no:].copy()
+    Fvv -= 0.5 * einsum('klcd,bdkl->bc',eri[:no,:no,no:,no:],t)
+
+    dt  = eri[no:,no:,:no,:no].copy()
     dt += einsum('bc,acij->abij',Fvv,t)
     dt += einsum('ac,cbij->abij',Fvv,t)
     dt -= einsum('kj,abik->abij',Foo,t)
     dt -= einsum('ki,abkj->abij',Foo,t)
 
-    dt += 0.5 * einsum('klij,abkl->abij',eris.oooo,t)
-    dt += 0.5 * einsum('abcd,cdij->abij',eris.vvvv,t)
-    tmp = 0.5 * einsum('klcd,cdij->klij',eris.oovv,t)
+    dt += 0.5 * einsum('klij,abkl->abij',eri[:no,:no,:no,:no],t)
+    dt += 0.5 * einsum('abcd,cdij->abij',eri[no:,no:,no:,no:],t)
+    tmp = 0.5 * einsum('klcd,cdij->klij',eri[:no,:no,no:,no:],t)
     dt += 0.5 * einsum('klij,abkl->abij',tmp,t)
 
-    tmp  = eris.ovvo.copy()
-    tmp += 0.5 * einsum('klcd,bdjl->kbcj',eris.oovv,t)
+    tmp  = eri[:no,no:,no:,:no].copy()
+    tmp += 0.5 * einsum('klcd,bdjl->kbcj',eri[:no,:no,no:,no:],t)
     tmp  = einsum('kbcj,acik->abij',tmp,t)
     tmp += tmp.transpose(1,0,3,2)
     tmp -= tmp.transpose(0,1,3,2)
     dt += tmp.copy()
     return dt
 
-def update_l(t, l, eris):
-    Foo  = eris.foo.copy()
-    Foo += 0.5 * einsum('ilcd,cdkl->ik',eris.oovv,t)
-    Fvv  = eris.fvv.copy()
-    Fvv -= 0.5 * einsum('klad,cdkl->ca',eris.oovv,t)
+def update_l(t, l, f, eri):
+    no = t.shape[3]
+
+    Foo  = f[:no,:no].copy()
+    Foo += 0.5 * einsum('ilcd,cdkl->ik',eri[:no,:no,no:,no:],t)
+    Fvv  = f[no:,no:].copy()
+    Fvv -= 0.5 * einsum('klad,cdkl->ca',eri[:no,:no,no:,no:],t)
     
-    dl  = eris.oovv.copy() 
+    dl  = eri[:no,:no,no:,no:].copy() 
     dl += einsum('ca,ijcb->ijab',Fvv,l)
     dl += einsum('cb,ijac->ijab',Fvv,l)
     dl -= einsum('ik,kjab->ijab',Foo,l)
     dl -= einsum('jk,ikab->ijab',Foo,l)
 
     tmp  = 0.5 * einsum('ilcd,cdkl->ik',l,t)
-    dl -= einsum('ik,kjab->ijab',tmp,eris.oovv)
-    dl -= einsum('jk,ikab->ijab',tmp,eris.oovv)
+    dl -= einsum('ik,kjab->ijab',tmp,eri[:no,:no,no:,no:])
+    dl -= einsum('jk,ikab->ijab',tmp,eri[:no,:no,no:,no:])
     tmp  = 0.5 * einsum('klad,cdkl->ca',l,t)
-    dl -= einsum('ca,ijcb->ijab',tmp,eris.oovv)
-    dl -= einsum('cb,ijac->ijab',tmp,eris.oovv)
+    dl -= einsum('ca,ijcb->ijab',tmp,eri[:no,:no,no:,no:])
+    dl -= einsum('cb,ijac->ijab',tmp,eri[:no,:no,no:,no:])
 
-    vvvv  = eris.vvvv.copy()
-    vvvv += 0.5 * einsum('klab,cdkl->cdab',eris.oovv,t)
-    oooo  = eris.oooo.copy()
-    oooo += 0.5 * einsum('ijcd,cdkl->ijkl',eris.oovv,t)
+    vvvv  = eri[no:,no:,no:,no:].copy()
+    vvvv += 0.5 * einsum('klab,cdkl->cdab',eri[:no,:no,no:,no:],t)
+    oooo  = eri[:no,:no,:no,:no].copy()
+    oooo += 0.5 * einsum('ijcd,cdkl->ijkl',eri[:no,:no,no:,no:],t)
 
     dl += 0.5 * einsum('cdab,ijcd->ijab',vvvv,l)
     dl += 0.5 * einsum('ijkl,klab->ijab',oooo,l)
 
-    ovvo  = eris.ovvo.copy()
-    ovvo += einsum('jlbd,cdkl->jcbk',eris.oovv,t)
+    ovvo  = eri[:no,no:,no:,:no].copy()
+    ovvo += einsum('jlbd,cdkl->jcbk',eri[:no,:no,no:,no:],t)
     tmp  = einsum('jcbk,ikac->ijab',ovvo,l)
     tmp += tmp.transpose(1,0,3,2)
     tmp -= tmp.transpose(0,1,3,2)
@@ -147,25 +151,21 @@ def compute_rdms(t, l, normal=False, symm=True):
         d2 = 0.5 * (d2 + d2.transpose(2,3,0,1).conj())
     return d1, d2
 
-def compute_kappa_intermediates(t, l, eris):
+def compute_kappa_intermediates(t, l, h, eri):
     d1, d2 = compute_rdms(t, l, normal=False, symm=True)
-    no, nv = eris.fov.shape 
+    no, _, nv, _ = l.shape 
 
-    Cov  = einsum('ba,ja->jb',d1[no:,no:],eris.hov.conj())
-    Cov -= einsum('ij,ib->jb',d1[:no,:no],eris.hov.conj())
-    Cov += 0.5 * einsum('abik,kija->jb',d2[no:,no:,:no,:no],eris.ooov)
-    Cov += 0.5 * einsum('abcd,jadc->jb',d2[no:,no:,no:,no:],eris.ovvv.conj())
-    Cov += einsum('ibak,jika->jb',d2[:no,no:,no:,:no],eris.ooov.conj())
-    Cov -= 0.5 * einsum('acjk,kbca->jb',d2[no:,no:,:no,:no],eris.ovvv)
-    Cov -= 0.5 * einsum('iljk,likb->jb',d2[:no,:no,:no,:no],eris.ooov.conj())
-    Cov -= einsum('kacj,kacb->jb',d2[:no,no:,no:,:no],eris.ovvv.conj())
+    Cov  = einsum('ba,aj->jb',d1[no:,no:],h[no:,:no]) 
+    Cov -= einsum('ij,bi->jb',d1[:no,:no],h[no:,:no])
+    Cov += 0.5 * einsum('pqjs,bspq->jb',eri[:,:,:no,:],d2[no:,:,:,:])
+    Cov -= 0.5 * einsum('bqrs,rsjq->jb',eri[no:,:,:,:],d2[:,:,:no,:])
 
     Aovvo  = einsum('ba,ij->jbai',np.eye(nv),d1[:no,:no])
     Aovvo -= einsum('ij,ba->jbai',np.eye(no),d1[no:,no:])
     return Aovvo, Cov
 
-def compute_kappa(t, l, eris):
-    Aovvo, Cov = compute_kappa_intermediates(t, l, eris)
+def compute_kappa(t, l, h, eri):
+    Aovvo, Cov = compute_kappa_intermediates(t, l, h, eri)
     no, nv = Cov.shape
     Aovvo = Aovvo.reshape(no*nv,no*nv)
     Cov = Cov.reshape(no*nv)
@@ -175,18 +175,19 @@ def compute_kappa(t, l, eris):
                       [kappa, np.zeros((nv,nv))]])
     return kappa
 
-
 def kernel_it1(mf, maxiter=1000, step=0.03, thresh=1e-6):
-    def kernel_t(eris):
-        eo = np.diag(eris.foo)
-        ev = np.diag(eris.fvv)
+    no = sum(mf.mol.nelec)
+
+    def kernel_t(f, eri):
+        eo = np.diag(f[:no,:no])
+        ev = np.diag(f[no:,no:])
         eia = lib.direct_sum('i-a->ia', eo, ev)
         eabij = lib.direct_sum('ia+jb->abij', eia, eia)
-        t = eris.oovv.transpose(2,3,0,1).conj()/eabij
+        t = eri[no:,no:,:no,:no]/eabij
 
         converged = False
         for i in range(maxiter):
-            dt = update_t(t,eris)
+            dt = update_t(t, f, eri)
             dnorm = np.linalg.norm(dt)
             t -= step * dt
 #            print('iter: {}, dnorm: {}'.format(i, dnorm))
@@ -197,12 +198,12 @@ def kernel_it1(mf, maxiter=1000, step=0.03, thresh=1e-6):
             print('t amplitude not converged!')
         return t
 
-    def kernel_l(eris, t):
+    def kernel_l(f, eri, t):
         l = t.transpose(2,3,0,1).copy()
 
         converged = False
         for i in range(maxiter):
-            dl = update_l(t,l,eris)
+            dl = update_l(t, l, f, eri)
             dnorm = np.linalg.norm(dl)
             l -= step * dl
 #            print('iter: {}, dnorm: {}'.format(i, dnorm))
@@ -213,31 +214,35 @@ def kernel_it1(mf, maxiter=1000, step=0.03, thresh=1e-6):
             print('l amplitude not converged!')
         return l
 
-    def compute_energy(eris, t, l):
-        e  = einsum('ii',eris.foo)
-        e -= 0.5 * einsum('ijij',eris.oooo)
-        e += 0.25 * einsum('ijab,abij',eris.oovv,t)
+    def compute_energy(f, eri, t):
+        e  = einsum('ii',f[:no,:no])
+        e -= 0.5 * einsum('ijij',eri[:no,:no,:no,:no])
+        e += 0.25 * einsum('ijab,abij',eri[:no,:no,no:,no:],t)
         return e.real
 
-    eris = ERIs(mf)
+    hao = mf.get_hcore()
+    fao = mf.get_fock()
+    eri_ao = mf.mol.intor('int2e_sph')
+
     mo_coeff = mf.mo_coeff.copy()
     e = mf.energy_elec()[0]
+    h, f, eri = ao2mo(hao, fao, eri_ao, mo_coeff)
 
     converged = False
     for i in range(maxiter):
-        t = kernel_t(eris)
-        l = kernel_l(eris, t)
-        e_new = compute_energy(eris, t, l)
+        t = kernel_t(f, eri)
+        l = kernel_l(f, eri, t)
+        e_new = compute_energy(f, eri, t)
         de, e = e_new - e, e_new
         if abs(de) < thresh:
             converged = True
             break
-        kappa = compute_kappa(t, l, eris)
+        kappa = compute_kappa(t, l, h, eri)
         dnorm = np.linalg.norm(kappa)
         print('iter: {}, dnorm: {}, de: {}, energy: {}'.format(i, dnorm, de, e))
         U = scipy.linalg.expm(step*kappa[::2,::2]) # U = U_{old,new}
         mo_coeff = np.dot(mo_coeff, U)
-        eris.update_hamiltonian(mo_coeff)
+        h, f, eri = ao2mo(hao, fao, eri_ao, mo_coeff)
     return t, l, mo_coeff, e 
 
 def kernel_it2(mf, maxiter=1000, step=0.03, thresh=1e-8):
@@ -276,40 +281,18 @@ def kernel_it2(mf, maxiter=1000, step=0.03, thresh=1e-8):
 #        t, l = rotate_amps(t, l, U)
     return t, l, mo_coeff 
 
-class ERIs:
-    def __init__(self, mf):
-        self.mf = mf
-        self.hao = mf.get_hcore()
-        self.fock_ao = mf.get_fock().astype(complex)
-        self.eri_ao = mf.mol.intor('int2e_sph').astype(complex)
+def ao2mo(hao, fao, eri_ao, mo_coeff):
+    nmo = mo_coeff.shape[0]
 
-        self.update_hamiltonian(mf.mo_coeff)
+    h = einsum('uv,up,vq->pq',hao,mo_coeff.conj(),mo_coeff)
+    h = sort1((h,h))
 
-    def update_hamiltonian(self, mo_coeff):
-        nmo = mo_coeff.shape[0]
-        no = sum(self.mf.mol.nelec)
+    f = einsum('uv,up,vq->pq',fao,mo_coeff.conj(),mo_coeff)
+    f = sort1((f,f))
 
-        h = einsum('uv,up,vq->pq',self.hao,mo_coeff.conj(),mo_coeff)
-        h = sort1((h,h))
-        self.hoo = h[:no,:no].copy()
-        self.hov = h[:no,no:].copy()
-        self.hvv = h[no:,no:].copy()
-
-        f0 = einsum('uv,up,vq->pq',self.fock_ao,mo_coeff.conj(),mo_coeff)
-        f0 = sort1((f0, f0))
-        self.foo = f0[:no,:no].copy()
-        self.fov = f0[:no,no:].copy()
-        self.fvv = f0[no:,no:].copy()
-
-        eri = einsum('uvxy,up,vr->prxy',self.eri_ao,mo_coeff.conj(),mo_coeff)
-        eri = einsum('prxy,xq,ys->prqs',eri,mo_coeff.conj(),mo_coeff)
-        eri = eri.transpose(0,2,1,3)
-        eri = sort2((eri, eri, eri), anti=False)
-        eri -= eri.transpose(0,1,3,2)
-        self.oooo = eri[:no,:no,:no,:no].copy() 
-        self.ooov = eri[:no,:no,:no,no:].copy()
-        self.oovv = eri[:no,:no,no:,no:].copy() 
-        self.ovvo = eri[:no,no:,no:,:no].copy() 
-        self.ovvv = eri[:no,no:,no:,no:].copy()
-        self.vvvv = eri[no:,no:,no:,no:].copy()
-
+    eri = einsum('uvxy,up,vr->prxy',eri_ao,mo_coeff.conj(),mo_coeff)
+    eri = einsum('prxy,xq,ys->prqs',eri,mo_coeff.conj(),mo_coeff)
+    eri = eri.transpose(0,2,1,3)
+    eri = sort2((eri, eri, eri), anti=False)
+    eri -= eri.transpose(0,1,3,2)
+    return h, f, eri
